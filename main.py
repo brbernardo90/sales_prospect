f6cf5c1bcefd1f763e55e4a585fcf0938535e835
Brayan Bernardo <brbernardo90@gmail.com>
2024-11-15 14:19:59 -0300

    add generate module

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from typing import Optional
 
 from fastapi import FastAPI
+from openai_generate import llm_generate_response
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    response = llm_generate_response("Hello World")
+    return {"message": response}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
---
+++ openai_generate.py
@@ -0,0 +1,19 @@
+import openai
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
+client = openai.OpenAI()
+
+def llm_generate_response(prompt: str):
+    prompt = [{'role': 'system', 'content': 'You are an AI HR assistant which helps to select the best candidated to the given job.'},
+              {'role': 'user',   'content': prompt}]
+
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=prompt,
+    )
+
+    return response.choices[0].message.content
