22460c364f15c6d13c82cef13a6fe81572332620
Brayan Bernardo <brbernardo90@gmail.com>
2024-11-15 16:31:00 -0300

    add cors

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 from openai_generate import llm_generate_response
 from scrapy import scrape_website
 
 app = FastAPI()
+
+# Configure CORS settings
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://67379fc7568f313ab68bd4cf--sales-prospect.netlify.app/"],  # List the front-end origins allowed to access the API
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers
+)
 
 # Request schema
 class SalesRequest(BaseModel):
